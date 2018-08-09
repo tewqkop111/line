8b65b518b4d2145860fa2b4f58bd57ed18c94a35
Chen,Hong-Wei <tewqkop111@gmail.com>
2018-08-09 14:28:15 +0800

    no message

---
+++ index.js
@@ -0,0 +1,9 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1599607215,
+  channelSecret: 04add1026dfdd8e466db860a631f8469,
+  channelAccessToken: NHaRGONDj0Xv4F9ny8FIWKxlSdFaoosZd4y/NwZeoArCeCp2RX7cP4OWXGvfNQI71xg4I9QqwZOH13G4MuPlcIY2xN9Q8i8/Rr/fc7vxaZPoR4WaW6K6nfDu3CcTDe00gp+oVqqiTMs5Ip2qkkhd3QdB04t89/1O/w1cDnyilFU=
+
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
